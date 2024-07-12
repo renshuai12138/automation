9d73f4704c6e6ba7eefba80604afdaccbbae2502
CHINA\r30064210 <2992321334@qq.com>
2024-07-12 08:49:43 +0800

    "444"

--- test3.py
+++ test3.py
@@ -1,32 +1,32 @@
-from appium import webdriver
-import unittest
-import time
+# from appium import webdriver
+# import unittest
+# import time
 
 
-class MyTestCase(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        server_url = 'http://localhost:4723/wd/hub'
-        desired_caps = {}
-        desired_caps['platformName'] = 'Android'
-        desired_caps['platformVersion'] = '12'
-        desired_caps['deviceName'] = '2c7c688a'
-        desired_caps['appPackage'] = 'tv.danmaku.bili'  # 应用的包名
-        desired_caps['appActivity'] = '.MainActivityV2'  # 应用的主Activity
-        desired_caps['automationName'] = 'UiAutomator2'
-        cls.driver = webdriver.Remote(server_url, desired_caps)
+# class MyTestCase(unittest.TestCase):
+#     @classmethod
+#     def setUpClass(cls):
+#         server_url = 'http://localhost:4723/wd/hub'
+#         desired_caps = {}
+#         desired_caps['platformName'] = 'Android'
+#         desired_caps['platformVersion'] = '12'
+#         desired_caps['deviceName'] = '2c7c688a'
+#         desired_caps['appPackage'] = 'tv.danmaku.bili'  # 应用的包名
+#         desired_caps['appActivity'] = '.MainActivityV2'  # 应用的主Activity
+#         desired_caps['automationName'] = 'UiAutomator2'
+#         cls.driver = webdriver.Remote(server_url, desired_caps)
 
-    @classmethod
-    def tearDownClass(cls):
-        cls.driver.quit()
+#     @classmethod
+#     def tearDownClass(cls):
+#         cls.driver.quit()
 
-    def test_find_and_click_element(self):
-        # 假设有一个id为my_button的按钮
-        # button = self.driver.find_element_by_id('tv.danmaku.bili:id/container')
-        # button.click()
-        self.assertTrue(True, "Button click action performed")
-        time.sleep(15)
+#     def test_find_and_click_element(self):
+#         # 假设有一个id为my_button的按钮
+#         # button = self.driver.find_element_by_id('tv.danmaku.bili:id/container')
+#         # button.click()
+#         self.assertTrue(True, "Button click action performed")
+#         time.sleep(15)
 
 
-if __name__ == '__main__':
-    unittest.main()
+# if __name__ == '__main__':
+#     unittest.main()
